820ac0d55dfc610ae5888fa3dea7de8c76eb6176
Hoang <hoangnguyen@bravesoft.vn>
2016-12-09 17:39:09 +0700

    Try

---
+++ scripts/BaseDb.js
@@ -0,0 +1,9 @@
+class BaseDb {
+	create();
+	delete();
+	add();
+}
+
+module.exports = BaseDb;
+
+// export let basedb = new BaseDb();
---
+++ scripts/DynamoDb.js
@@ -0,0 +1,22 @@
+import { BaseDb } from "./BaseDb.js";
+
+// class DynamoDb extends BaseDb {
+	
+// 	create()
+// 	{
+// 		console.log("create");
+// 	}
+
+// 	delete()
+// 	{
+// 		console.log("delete");
+// 	}
+
+// 	add()
+// 	{
+// 		console.log("add");
+// 	}
+// }
+
+// var db = new DynamoDb();
+// db.create();
