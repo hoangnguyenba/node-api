68cb403e0a6562625259471392e9c92da74ee025
Hoang Nguyen <hoang.nguyenba@hotmail.com>
2016-12-10 17:07:08 +0700

    Clean

--- scripts/dynamodb.js
+++
@@ -1,6 +0,0 @@
-var config = require('../dist/config/config');
-var AWS = require("aws-sdk");
-AWS.config.update(config.database);
-
-exports.dynamoDB = new AWS.DynamoDB();
-exports.docClient = new AWS.DynamoDB.DocumentClient();
